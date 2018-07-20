971668ea7942d5b5e26ce2b03cb982654e0af6fc
Miguel Ruiz <mig.ruiz@gmail.com>
2018-07-20 23:10:54 +0100

    working project

--- App/sqliteSensorReadingRepository.js
+++ App/sqliteSensorReadingRepository.js
@@ -1,5 +1,4 @@
 var db = require('./db-sqlite.js').database();
-var db2 = require('./db-sqlite.js').database();
 exports.insertReadingAsync =async function (reading) {
 
     await db.runAsync("REPLACE INTO ZonesTemperature(zoneCode,sensorId,channel,temperature,humidity,timestamp) values ($zoneCode,$sensorId,$channel,$temperature,$humidity,$timestamp)",
